f5d75ce0b19de5125f7101c66edaca8e55452ecb
ssh-lvl <sshlvl.contact@gmail.com>
2025-09-15 14:05:00 -0700

    password to key encryption

--- js/crypt/crypt.js
+++ js/crypt/crypt.js
@@ -38,3 +38,52 @@ export async function getCryptModule() {
     cachedModule.hmacSha256 = hmacSha256;
     return cachedModule;
 }
+
+export async function passwordToKey(password, saltStr = "CTDPMZOODz") {
+  const encoder = new TextEncoder();
+  const passwordData = encoder.encode(password);
+  const salt = encoder.encode(saltStr);
+
+  const baseKey = await crypto.subtle.importKey(
+    "raw",
+    passwordData,
+    { name: "PBKDF2" },
+    false,
+    ["deriveBits"]
+  );
+
+  const rawBits = await crypto.subtle.deriveBits(
+    {
+      name: "PBKDF2",
+      salt: salt,
+      iterations: 100000,
+      hash: "SHA-256"
+    },
+    baseKey,
+    256
+  );
+
+  const bytes = new Uint8Array(rawBits);
+  const base64Key = btoa(String.fromCharCode(...bytes));
+
+  return base64Key;
+}
+
+const storedKey = await importKeyFromBase64(localStorage.getItem("encryptionKey"));
+
+export async function importKeyFromBase64(base64) {
+  if (base64 != "" && base64 != null) {
+    const binary = atob(base64);
+    const bytes = new Uint8Array(binary.length);
+    for (let i = 0; i < binary.length; i++) {
+      bytes[i] = binary.charCodeAt(i);
+    }
+    return crypto.subtle.importKey(
+      "raw",
+      bytes,
+      { name: "AES-GCM" },
+      false,
+      ["encrypt", "decrypt"]
+    );
+  }
+}
