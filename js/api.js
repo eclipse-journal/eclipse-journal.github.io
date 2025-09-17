3fe831e90d12d5717fa819903f55883d5e28559d
ssh-lvl <sshlvl.contact@gmail.com>
2025-09-17 13:52:12 -0700

    api fetch is broken, needs fixed

--- js/api.js
+++ js/api.js
@@ -1,19 +1,17 @@
 export async function api(body, auth_token, endpoint) {
-    const Headers = new Headers();
-    Headers.append("Content-Type", "application/json");
-    Headers.append("Authorization", `Bearer ${auth_token}`);
-
-    const raw = JSON.stringify(body);
-
-    const requestOptions = {
-        method: "POST",
-        headers: Headers,
-        body: raw,
-        redirect: "follow"
-    };
-
     try {
-        const response = await fetch(`https://szzslijzpkujryqivoxn.supabase.co/functions/v1/${endpoint}`, requestOptions);
+        const response = await fetch(
+            `https://szzslijzpkujryqivoxn.supabase.co/functions/v1/${endpoint}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${auth_token}`
+                },
+                body: JSON.stringify(body),
+                redirect: "follow"
+            }
+        );
         return await response.text();
     } catch (error) {
         console.error(error);
